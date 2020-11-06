e902f5459d1c54816b331e8168581682bb431613
Yorrick Bakker <y.bakker@me.com>
2020-11-06 19:38:08 +0100

    Add AWS config

--- api/app.js
+++ api/app.js
@@ -1,13 +1,21 @@
+const AWS = require('aws-sdk')
+AWS.config = new AWS.Config({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    sessionToken: process.env.AWS_SESSION_TOKEN,
+  },
+  region: process.env.AWS_REGION,
+})
+
 const express = require('express')
-const path = require('path')
 const cookieParser = require('cookie-parser')
 const morgan = require('morgan')
 const logger = require('debug')('http')
 const serverless = require('serverless-http')
+const app = express()
 
 const slugsRouter = require('./routes/slugs')
-
-const app = express()
 
 app.use(morgan('dev', { stream: { write: msg => logger(msg) } }))
 app.use(express.json())

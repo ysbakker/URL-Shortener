ce4a670de101fa453184c716fb90a78ed1a5e558
Yorrick Bakker <y.bakker@me.com>
2020-11-07 01:47:00 +0100

    Add method for getting slug by URL

--- api/model/slugs.js
+++ api/model/slugs.js
@@ -1,13 +1,37 @@
-const isEmpty = require('../util/isEmpty')
 const AWS = require('aws-sdk')
+const { unmarshall } = AWS.DynamoDB.Converter
 const dynamo = new AWS.DynamoDB()
+const isEmpty = require('../util/isEmpty')
+const stripUrl = require('../util/stripUrl')
+const log = require('debug')('log')
 
 const getSlugData = async slug => {
   const result = await dynamo
-    .getItem({ TableName: 'Slugs', Key: { slug: { S: slug } } })
+    .getItem({
+      TableName: 'Slugs',
+      Key: { slug: { S: slug } },
+    })
     .promise()
 
-  return isEmpty(result) ? null : result
+  return isEmpty(result) ? null : unmarshall(result.Item)
 }
 
-module.exports = { getSlugData }
+const getSlugByURL = async url => {
+  const result = await dynamo
+    .query({
+      TableName: 'Slugs',
+      IndexName: 'url-index',
+      KeyConditionExpression: '#U = :url',
+      ExpressionAttributeNames: { '#U': 'url' },
+      ExpressionAttributeValues: {
+        ':url': {
+          S: stripUrl(url).url,
+        },
+      },
+    })
+    .promise()
+
+  return unmarshall(result.Items[0]) || null
+}
+
+module.exports = { getSlugData, getSlugByURL }

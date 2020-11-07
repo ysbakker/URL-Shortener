86329818c8b597e7ee68d1a2ee0e6779d85d8089
Yorrick Bakker <y.bakker@me.com>
2020-11-07 01:47:16 +0100

    Fix GET slug endpoint

--- api/routes/slugs.js
+++ api/routes/slugs.js
@@ -5,11 +5,29 @@ const slugs = require('../model/slugs')
 
 router.get('/:slug', async (req, res, next) => {
   const { slug } = req.params
-  const result = await slugs.getSlugData(slug)
+  let result
+  try {
+    result = await slugs.getSlugData(slug)
+  } catch (e) {
+    return next({ rescode: 500, message: 'Slug data provider error occurred.' })
+  }
 
-  if (!result) return res.status(404).send()
+  if (!result) return next({ rescode: 404, message: 'Slug does not exist!' })
 
-  res.json(result.Item)
+  res.json(result)
+})
+
+router.post('/', async (req, res, next) => {
+  const { url } = req.body
+
+  try {
+    const slug = await slugs.getSlugByURL(url)
+    if (slug) return res.json(slug)
+  } catch (e) {
+    return next({ rescode: 500, message: 'Slug data provider error occurred.' })
+  }
+
+  res.json(url)
 })
 
 module.exports = router

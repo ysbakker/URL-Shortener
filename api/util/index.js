6da41e48e7927bf1a7522fc73547853360a74319
Yorrick Bakker <y.bakker@me.com>
2020-11-08 19:12:21 +0100

    Bundle utility functions

---
+++ api/util/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  parseMeta: require('./parseMeta'),
+  isEmpty: require('./isEmpty'),
+  stripURL: require('./stripURL'),
+  generateSlug: require('./generateSlug'),
+  isURL: require('./isURL'),
+}

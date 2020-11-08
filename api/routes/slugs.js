005bc0a66fcdfa19b65508b2906e01177b4a01b5
Yorrick Bakker <y.bakker@me.com>
2020-11-08 19:03:25 +0100

    Add edge cases to slug post

--- api/routes/slugs.js
+++ api/routes/slugs.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router()
 const log = require('debug')('log')
 const slugs = require('../data/slugs')
+const $ = require('../util/index')
 
 router.get('/:slug', async (req, res, next) => {
   const { slug } = req.params
+
   let result
   try {
     result = await slugs.getSlugData(slug)
@@ -19,15 +21,24 @@ router.get('/:slug', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
   const { url } = req.body
+  if (!url) return next({ rescode: 400, message: 'No url specified' })
+  if (!$.isUrl(url))
+    return next({ rescode: 400, message: 'Specified url is invalid' })
 
   try {
-    const slug = await slugs.getSlugByURL(url)
-    if (slug) return res.json(slug)
-  } catch (e) {
-    return next({ rescode: 500, message: 'Slug data provider error occurred.' })
-  }
+    let slug = await slugs.getSlugByURL(url)
+    if (slug) return res.status(200).json(slug)
 
-  res.json(url)
+    return res.status(201).json(await slugs.createSlug(url))
+  } catch (e) {
+    return next({
+      rescode: 500,
+      message:
+        e.code === 'SLUG_GENERATOR_ERROR'
+          ? e.message
+          : 'Slug data provider error occurred.',
+    })
+  }
 })
 
 module.exports = router

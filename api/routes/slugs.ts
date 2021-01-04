da4cd44331644c054e31d08a04d43a8fe9de2ebe
Yorrick Bakker <y.bakker@me.com>
2021-01-04 22:49:31 +0100

    Migrate routes to ts

--- api/routes/slugs.ts
+++ api/routes/slugs.ts
@@ -1,16 +1,17 @@
-const express = require('express')
+import { Router } from 'express'
 
-const router = express.Router()
-const log = require('debug')('log')
-const slugs = require('../data/slugs')
-const $ = require('../util/index')
+const router = Router()
+import debug from 'debug'
+import { getSlugData, getSlugByURL, createSlug } from '../data/slugs'
+import { isURL } from '../util/index'
+const log = debug('log')
 
 router.get('/:slug', async (req, res, next) => {
   const { slug } = req.params
 
   let result
   try {
-    result = await slugs.getSlugData(slug)
+    result = await getSlugData(slug)
   } catch (e) {
     return next({ rescode: 500, message: 'Slug data provider error occurred.' })
   }
@@ -23,14 +24,14 @@ router.get('/:slug', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
   const { url } = req.body
   if (!url) return next({ rescode: 400, message: 'No url specified' })
-  if (!$.isURL(url))
+  if (!isURL(url))
     return next({ rescode: 400, message: 'Specified url is invalid' })
 
   try {
-    const slug = await slugs.getSlugByURL(url)
+    const slug = await getSlugByURL(url)
     if (slug) return res.status(200).json(slug)
 
-    return res.status(201).json(await slugs.createSlug(url))
+    return res.status(201).json(await createSlug(url))
   } catch (e) {
     return next({
       rescode: 500,
@@ -42,4 +43,4 @@ router.post('/', async (req, res, next) => {
   }
 })
 
-module.exports = router
+export default router

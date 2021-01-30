9fd7ec10154075bd2329130cc1d4d7198d297707
Yorrick Bakker <y.bakker@me.com>
2021-01-30 16:13:12 +0100

    Change non-null assertion

--- api/app.ts
+++ api/app.ts
@@ -9,8 +9,8 @@ const logger = debug('http')
 
 AWS.config = new AWS.Config({
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
     sessionToken: process.env.AWS_SESSION_TOKEN,
   },
   region: process.env.AWS_REGION,

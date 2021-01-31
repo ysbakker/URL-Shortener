6ff3485c7e656f620c5175ea525c1b8db17fc1a4
Yorrick Bakker <y.bakker@me.com>
2021-01-31 18:44:31 +0100

    Show slug and allow copy in UrlPreview

--- src/components/UrlPreview.vue
+++ src/components/UrlPreview.vue
@@ -1,19 +1,56 @@
 <template>
-  <div>
-    <ClipLoader v-if="fetching" color="#9191da" />
-    <h1 v-if="slug && !fetching">{{ `${domain}/${slug}` }}</h1>
+  <div class="url-preview">
+    <div>
+      <ClipLoader v-if="fetching" color="#9191da" />
+      <a
+        class="shortlink"
+        :href="`https://${domain}/${slug}`"
+        target="_blank"
+        v-if="slug && !fetching"
+      >
+        <span id="slug">
+          {{ `${domain}/${slug}` }}
+        </span>
+        <OpenInNew />
+      </a>
+    </div>
+    <button
+      type="button"
+      @click="handleCopy"
+      id="copy-btn"
+      data-clipboard-target="#slug"
+    >
+      <span v-if="copySuccess === true"><CheckCircle /> Copied</span>
+      <span v-else><ContentCopy /> Copy</span>
+    </button>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
 import ClipLoader from 'vue-spinner/src/ClipLoader.vue'
+import OpenInNew from 'icons/OpenInNew.vue'
+import ContentCopy from 'icons/ContentCopy.vue'
+import CheckCircle from 'icons/CheckCircle.vue'
+import ClipboardJS from 'clipboard'
 
 @Component({
-  components: { ClipLoader },
+  components: { ClipLoader, OpenInNew, ContentCopy, CheckCircle },
 })
 export default class UrlPreview extends Vue {
   domain = process.env.VUE_APP_DOMAIN
+  copySuccess = false
+
+  handleCopy(e: Event) {
+    e.preventDefault()
+    this.copySuccess = false
+    const cb = new ClipboardJS('#copy-btn')
+
+    cb.on('success', (e: ClipboardJS.Event) => {
+      e.clearSelection()
+      this.copySuccess = true
+    })
+  }
 
   get fetching() {
     return this.$store.state.url.fetching
